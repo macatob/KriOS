a578b78db13dcba11a8ba571bd2a5849d0c3ab85
iumix <6557185+iumix@user.noreply.gitee.com>
2020-08-04 21:12:04 +0800

    V0.0.6

--- asmlib.nas
+++ asmlib.nas
@@ -2,13 +2,76 @@
 ; tab=4
 
 [FORMAT "WCOFF"]
+[INSTRSET "i486p"]
 [BITS 32]
 
 [FILE "asmlib.nas"]     ;file name
-    GLOBAL _io_hlt      ;method name
+    GLOBAL _io_hlt, _io_cli, _io_sti, io_stihlt
+    GLOBAL _io_in8, _io_in16, _in_in32
+    GLOBAL _io_out8, _io_out16, _io_out32
+    GLOBAL _io_load_eflags, _io_store_eflags
 
 [SECTION .text]
 
 _io_hlt:    ;void io_hlt(void)
     hlt
+    ret
+
+_io_cli:    ;void io_cli(void)
+    cli
+    ret
+    
+_io_sti:    ;void io_sti(void)
+    sti
+    ret
+
+_io_stihlt:    ;void io_stihlt(void)
+    sti
+    hlt
+    ret
+
+_io_in8:    ;void io_in8(int port);
+    mov edx, [esp+4]    ;port
+    mov eax, 0
+    in al, dx
+    ret
+
+_io_in16:    ;void io_in16(int port);
+    mov edx, [esp+4]    ;port
+    mov eax, 0
+    in ax, dx
+    ret
+
+_io_in32:    ;void io_in32(int port);
+    mov edx, [esp+4]    ;port
+    mov eax, 0
+    in eax, dx
+    ret
+
+_io_out8:    ;void io_out8(int port, int data);
+    mov edx, [esp+4]    ;port
+    mov eax, [esp+8]    ;data
+    out dx, al
+    ret
+
+_io_out16:    ;void io_out16(int port, int data);
+    mov edx, [esp+4]    ;port
+    mov eax, [esp+8]    ;data
+    out dx, ax
+    ret
+
+_io_out32:    ;void io_out32(int port, int data);
+    mov edx, [esp+4]    ;port
+    mov eax, [esp+8]    ;data
+    out dx, eax
+    ret
+
+_io_load_eflags:    ;void io_load_eflags(void);
+    pushfd
+    pop eax
+    ret
+_io_store_eflags:   ;void io_store_eflgas(void);
+    mov eax, [esp+4]
+    push eax
+    popfd
     ret
--- bootpack.c
+++ bootpack.c
@@ -1,10 +1,65 @@
 void io_hlt(void);
+void io_cli(void);
+void io_out8(int port, int data);
+int io_load_eflags(void);
+void io_store_eflags(int eflags);
+void init_palette(void);
+void set_palette(int start, int end, unsigned char *rgb);
 
 void HariMain(void)
 {
+    int i;
+    char *p;
 
-fin:
-    io_hlt();
-    goto fin;
+    init_palette();
 
+    p = (char *) 0xa0000;
+
+    for (i = 0; i <= 0xffff; i++) {
+        p[i] = i & 0x0f;
+    }
+    
+    while (1) {
+        io_hlt();
+    }
+}
+
+void init_palette(void)
+{
+    static unsigned char table_rgb[16 * 3] = {
+        0x00, 0x00, 0x00,   /* 纯黑 */
+        0xff, 0x00, 0x00,   /* 亮红 */
+        0x00, 0xff, 0x00,   /* 亮绿 */
+        0xff, 0xff, 0x00,   /* 亮黄 */
+        0x00, 0x00, 0xff,   /* 亮蓝 */
+        0xff, 0x00, 0xff,   /* 亮紫 */
+        0x00, 0xff, 0xff,   /* 浅亮蓝 */ 
+        0xff, 0xff, 0xff,   /* 纯白 */ 
+        0xc6, 0xc6, 0xc6,   /* 亮灰 */ 
+        0x84, 0x00, 0x00,   /* 暗红 */ 
+        0x00, 0x84, 0x00,   /* 暗绿 */ 
+        0x84, 0x84, 0x00,   /* 暗黄 */
+        0x00, 0x00, 0x84,   /* 暗青 */
+        0x84, 0x00, 0x84,   /* 暗紫 */
+        0x00, 0x84, 0x84,   /* 浅暗蓝 */
+        0x84, 0x84, 0x84    /* 暗灰 */
+    };
+    set_palette(0, 15, table_rgb);
+    return;
+}
+
+void set_palette(int start, int end, unsigned char *rgb)
+{
+    int i, eflags;
+    eflags = io_load_eflags();
+    io_cli();
+    io_out8(0x03c8, start);
+    for (i = start; i <= end; i++) {
+        io_out8(0x03c9, rgb[0] / 4);
+        io_out8(0x03c9, rgb[1] / 4);
+        io_out8(0x03c9, rgb[2] / 4);
+        rgb += 3;
+    }
+    io_store_eflags(eflags);
+    return;
 }
